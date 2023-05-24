c79b86490999d343a3e7f9826fef0ba080d43bad
richardwaters9049 <119029825+richardwaters9049@users.noreply.github.com>
2023-05-24 03:02:26 +0100

    removal of comments

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -33,9 +33,6 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
-      {/* {alert(session?.user)} */}
-      {/* {alert(providers)} */}
-
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
         {session?.user ? (
